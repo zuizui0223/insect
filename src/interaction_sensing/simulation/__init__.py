c63081632ea85ebfde2eca2334e860bcd48ca6c7
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-07-02 20:31:59 +0900

    feat: expose temporal nuisance-risk benchmark API

--- src/interaction_sensing/simulation/__init__.py
+++ src/interaction_sensing/simulation/__init__.py
@@ -16,6 +16,22 @@ from .latent_evaluation import (
     evaluate_latent_results,
     write_latent_evaluation,
 )
+from .temporal_risk import (
+    TemporalRiskModel,
+    TemporalRiskModelConfig,
+    TemporalRiskTrainingSummary,
+    fit_temporal_risk_model,
+    temporal_reference_matrix,
+)
+from .temporal_risk_benchmark import (
+    TemporalRiskBenchmarkConfig,
+    TemporalRiskBenchmarkResult,
+    TemporalRiskCalibration,
+    TemporalRiskPolicy,
+    TemporalRiskRun,
+    run_temporal_risk_benchmark,
+    write_temporal_risk_benchmark,
+)
 from .visual_benchmark import (
     VisualBenchmarkConfig,
     VisualBenchmarkResult,
@@ -43,6 +59,14 @@ __all__ = [
     "ReferenceMode",
     "RenderedFrame",
     "ScenarioConfig",
+    "TemporalRiskBenchmarkConfig",
+    "TemporalRiskBenchmarkResult",
+    "TemporalRiskCalibration",
+    "TemporalRiskModel",
+    "TemporalRiskModelConfig",
+    "TemporalRiskPolicy",
+    "TemporalRiskRun",
+    "TemporalRiskTrainingSummary",
     "VisualBenchmarkConfig",
     "VisualBenchmarkResult",
     "VisualDisturbanceWorld",
@@ -52,11 +76,15 @@ __all__ = [
     "estimate_global_shift",
     "evaluate_latent_results",
     "extract_visual_features",
+    "fit_temporal_risk_model",
     "run_benchmark",
     "run_latent_benchmark",
+    "run_temporal_risk_benchmark",
     "run_visual_benchmark",
+    "temporal_reference_matrix",
     "write_benchmark",
     "write_latent_benchmark",
     "write_latent_evaluation",
+    "write_temporal_risk_benchmark",
     "write_visual_benchmark",
 ]
