4d290f952932ee2dcb611e5ffa4d51afeec528b6
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-07-02 19:41:19 +0900

    feat: expose paired latent benchmark evaluator

--- src/interaction_sensing/simulation/__init__.py
+++ src/interaction_sensing/simulation/__init__.py
@@ -9,20 +9,32 @@ from .latent_benchmark import (
     write_latent_benchmark,
 )
 from .latent_disturbance import LatentDisturbanceConfig, LatentDisturbanceWorld, ReferenceMode
+from .latent_evaluation import (
+    FailureMapCell,
+    LatentEvaluationConfig,
+    PairedEffect,
+    evaluate_latent_results,
+    write_latent_evaluation,
+)
 from .world import ScenarioConfig
 
 __all__ = [
     "BenchmarkConfig",
     "BenchmarkResult",
+    "FailureMapCell",
     "LatentBenchmarkConfig",
     "LatentBenchmarkResult",
     "LatentDisturbanceConfig",
     "LatentDisturbanceWorld",
+    "LatentEvaluationConfig",
     "LatentPolicy",
+    "PairedEffect",
     "ReferenceMode",
     "ScenarioConfig",
+    "evaluate_latent_results",
     "run_benchmark",
     "run_latent_benchmark",
     "write_benchmark",
     "write_latent_benchmark",
+    "write_latent_evaluation",
 ]
