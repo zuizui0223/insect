a024e0688974a85c13fd5364bf84cc62911782d3
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-07-02 16:28:01 +0900

    feat: export target-relative motion primitives

--- src/interaction_sensing/sensing/__init__.py
+++ src/interaction_sensing/sensing/__init__.py
@@ -2,5 +2,18 @@
 
 from .motion import MOG2MotionExtractor
 from .scene_state import SceneStateEstimator
+from .stabilise import (
+    TargetMotionEstimator,
+    TargetPose,
+    relative_motion_magnitude,
+    translate_bbox,
+)
 
-__all__ = ["MOG2MotionExtractor", "SceneStateEstimator"]
+__all__ = [
+    "MOG2MotionExtractor",
+    "SceneStateEstimator",
+    "TargetMotionEstimator",
+    "TargetPose",
+    "relative_motion_magnitude",
+    "translate_bbox",
+]
